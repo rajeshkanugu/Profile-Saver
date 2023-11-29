fb88e1eca4f7f5192f6e05edc6084487f73be7c6
kanugurajesh <245122733172@mvsrec.edu.in>
2023-11-29 18:23:06 +0530

    Adding styling to the profile page

--- src/app/myprofile/page.tsx
+++ src/app/myprofile/page.tsx
@@ -9,12 +9,14 @@ const imageStyle = {
 const Home = async () => {
     const session = await getServerSession(options);    
     return (
-        <main className="w-screen h-screen items-center justify-center flex text-lg flex-col gap-10 font-bold">
-            <h1 className="text-2xl">My Profile</h1>
-            <div className="flex gap-4 flex-col justify-center items-center">
-                {session?.user?.image && <Image src={session?.user?.image as string} alt="user image" width={180} height={180} style={imageStyle} />}
-                {session?.user?.name && <span className="font-bold mt-3">{session?.user?.name}</span>}
-                {session?.user?.email && <span className="font-bold">{session?.user?.email}</span>}
+        <main className="w-screen h-screen text-lg items-center justify-center flex">
+            <div className="items-center justify-center flex gap-10 font-bold flex-col border-2 p-10 rounded-xl shadow-lg">
+                <h1 className="text-2xl">My Profile</h1>
+                <div className="flex gap-4 flex-col justify-center items-center">
+                    {session?.user?.image && <Image src={session?.user?.image as string} alt="user image" width={180} height={180} style={imageStyle} />}
+                    {session?.user?.name && <span className="font-bold mt-5">{session?.user?.name}</span>}
+                    {session?.user?.email && <span className="font-bold">{session?.user?.email}</span>}
+                </div>
             </div>
         </main>
     );
