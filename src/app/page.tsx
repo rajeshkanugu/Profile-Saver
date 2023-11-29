62494159dca30d304aa2b17463647da042a6f5c5
kanugurajesh <245122733172@mvsrec.edu.in>
2023-11-29 18:38:12 +0530

    Updating the home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,10 +6,23 @@ const Home = async () => {
     const session = await getServerSession(options);    
     return (
         <main>
-            <h1>Home</h1>
-            <p>
-                {session?.user?.image && <Image src={session?.user?.image as string} alt="user image" width="100" height="100" />}
-            </p>
+            <nav>
+                <div className="flex justify-between items-center py-6 px-10">
+                    <div className="flex gap-4 items-center">
+                        <div className="flex gap-4 items-center">
+                            <a href="/">
+                                <Image src="/profile.png" alt="logo" width={40} height={40} />
+                            </a>
+                            <a href="/" className="text-2xl font-bold">Profile Saver</a>
+                        </div>
+                    </div>
+                    <div className="flex gap-4 items-center">
+                        {!session && <a href="/api/auth/signin" className="text-lg font-bold">Sign in</a>}
+                        {session && <a href="/myprofile" className="text-lg font-bold">My Profile</a>}
+                        {session && <a href="/api/auth/signout" className="text-lg font-bold">Sign out</a>}
+                    </div>
+                </div>
+            </nav>
         </main>
     );
 };
